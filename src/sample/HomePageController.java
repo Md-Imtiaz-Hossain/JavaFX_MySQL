4eac8b38ee3db8f9844a7fe22701360e1d8ad86a
Imtiaz <imtiaz005123@gmail.com>
2020-09-11 21:25:53 +0600

    Home Page Complete

---
+++ src/sample/HomePageController.java
@@ -0,0 +1,95 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+
+public class HomePageController  {
+    @FXML
+    private Button signIn;
+    @FXML
+    private Button showPasswordButton;
+    @FXML
+    private Button signUp;
+    @FXML
+    private Button signUpForBankAccount;
+    @FXML
+
+
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameTextField;
+
+
+    @FXML
+    private Label showPass;
+
+
+    public void showPasswordButtonOnAction(ActionEvent event){
+        String password = passwordField.getText();
+        showPass.setText("Password- " + password);
+    }
+
+
+    public void signInOnAction(javafx.event.ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("SignIn");
+            Scene scene = new Scene(root, 700, 900); //"/image/login.png"
+            scene.getStylesheets().add("/Style/style.css");
+            primaryStage.initModality(Modality.APPLICATION_MODAL); // Disable Others all Window
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void signUpForBankAccountOnAction(ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("SignUp");
+            Scene scene = new Scene(root, 700, 900); //"/image/login.png"
+            scene.getStylesheets().add("/Style/style.css");
+            primaryStage.initModality(Modality.APPLICATION_MODAL); // Disable Others all Window
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void signUpOnAction(javafx.event.ActionEvent event) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("SignUp");
+            Scene scene = new Scene(root, 700, 900); //"/image/login.png"
+            scene.getStylesheets().add("/Style/style.css");
+            primaryStage.initModality(Modality.APPLICATION_MODAL); // Disable Others all Window
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        primaryStage.setTitle("ABCD BANK");
+        Scene scene = new Scene(root, 800, 800); //"/image/login.png"
+        scene.getStylesheets().add("/Style/style.css");
+        primaryStage.setScene(scene);
+        //primaryStage.resizableProperty().setValue(false); // Disable Mazimuise Button
+        // primaryStage.initStyle(StageStyle.UTILITY); // Disable Mazimuise & Minimice Button
+        primaryStage.show();
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
