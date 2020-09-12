16fbb7a22f25d4dfb4cfd4439cb2c29d5796885c
Imtiaz <imtiaz005123@gmail.com>
2020-09-12 13:31:53 +0600

    Account Type ChoiceBox Code Implementation & Cancel Button Code Implementation  is complete in CreateAccount.FXML,,,

--- src/sample/CreateBankAccountController.java
+++ src/sample/CreateBankAccountController.java
@@ -1,5 +1,44 @@
 package sample;
 
-public class CreateBankAccountController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class CreateBankAccountController implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> accountTypeChoiceBox;
+
+    @FXML
+    private Button cancelButton;
+
+
+    public void cancelButtonOnAction(ActionEvent event){
+        // get a handle to the stage
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        accountTypeChoiceBox.getItems().add("Saving Account");
+        accountTypeChoiceBox.getItems().add("General Account");
+        accountTypeChoiceBox.getItems().add("Student Account");
+        accountTypeChoiceBox.getItems().add("Business Account");
+        accountTypeChoiceBox.getItems().add("Joint Account");
+
+        accountTypeChoiceBox.setValue("General Account");
+    }
 
 }
